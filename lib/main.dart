fd87c1704bd7b4458bb7fc48f2e8b2566ef934c4
Chukwuebuka Onah <chukwuebuka@kobo360.com>
2023-04-22 22:06:07 +0100

    multiple user join

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jitsi_meet_wrapper/jitsi_meet_wrapper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const NewScreen(),
     );
   }
@@ -25,6 +24,31 @@ class NewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("App")),
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          ElevatedButton(
+            onPressed: ()=> setupCall(email: "csonah@gmail.com", avatar: "https://images.unsplash.com/photo-1616790876844-97c0c6057364?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXQlMjBnaXJsfGVufDB8fDB8fA%3D%3D&w=1000&q=80"),
+            child: const Text("Join Meeting as user 1"),
+          ),
+          ElevatedButton(
+            onPressed: ()=> setupCall(email: "cynthia@gmail.com", avatar: "https://burst.shopifycdn.com/photos/model-in-gold-fashion.jpg?width=1200&format=pjpg&exif=1&iptc=1"),
+            child: const Text("Join Meeting as user 2"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void setupCall({required String email, required String avatar}) async {
+    var options = JitsiMeetingOptions(
+      roomNameOrUrl: 'xyz-abc',
+      userEmail: email,
+      subject: "Random call",
+      userAvatarUrl: avatar
+    );
+    await JitsiMeetWrapper.joinMeeting(options: options);
   }
 }
